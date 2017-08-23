e0051d02d95d06bf949e6e7d764faacb1c7f8943
Maxwell Brown <maxwellgbrown@gmail.com>
2017-08-22 20:17:58 -0400

    Initial webserver exploration

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const helloWorld = (request, response) => {
+  
+  response.writeHead(200, {
+      'Content-Type': 'text/plain'
+  });
+
+  response.write('Hello World\n');
+
+  response.end();  // Tells server we're done writing the response
+}
+
+http.createServer(helloWorld).listen(1337);
