29575568a106c142d3e447943367f3b462124013
Maxwell Brown <maxwellgbrown@gmail.com>
2017-08-28 09:51:39 -0400

    unit tested views.helloWorld

--- tests/test_views.js
+++ tests/test_views.js
@@ -8,10 +8,30 @@ const test = require('ava');
 const views = require('../app/views');
 
 
-test('hello world', t => {
+test('helloWorld 200 OK', t => {
   let request = httpMocks.createRequest();
   let response = httpMocks.createResponse();
 
   views.helloWorld(request, response);
-  t.pass();
+  t.is(response.statusCode, 200);
+});
+
+
+test('helloWorld "Hello World!" as body', t => {
+  let request = httpMocks.createRequest();
+  let response = httpMocks.createResponse();
+
+  views.helloWorld(request, response);
+  const data = response._getData();
+  t.is(data, "Hello World!");
+});
+
+
+test('helloWorld calls .end()', t => {
+  let request = httpMocks.createRequest();
+  let response = httpMocks.createResponse();
+
+  views.helloWorld(request, response);
+  const data = response._getData();
+  t.is(response._isEndCalled(), true);
 });
