b5fc5eb41cc00fc048be1e8342d8ebbf847ad5ca
Maxwell Brown <maxwellgbrown@gmail.com>
2017-08-28 09:15:22 -0400

    included node-mocks-http! (finally found the right mocks!)
      https://github.com/howardabrams/node-mocks-http

--- tests/test_views.js
+++ tests/test_views.js
@@ -1,8 +1,17 @@
+const http = require('http');
 const path = require('path');
+const url = require('url');
 
+const httpMocks = require('node-mocks-http');
 const test = require('ava');
+
+const views = require('../app/views');
 
 
 test('hello world', t => {
+  let request = httpMocks.createRequest();
+  let response = httpMocks.createResponse();
+
+  views.helloWorld(request, response);
   t.pass();
 });
